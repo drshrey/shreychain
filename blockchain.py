15f70f8f89ba832813c35751f856ecb79b662a0c
Shreyas Jaganmohan <shreyas@checkr.com>
2017-12-08 15:43:50 -0800

    Add consensus algos

--- blockchain.py
+++ blockchain.py
@@ -7,6 +7,9 @@ from time import time
 import json
 import hashlib
 from uuid import uuid4
+import requests
+from urllib.parse import urlparse
+import sys
 
 from flask import Flask, jsonify, request
 
@@ -14,8 +17,52 @@ from flask import Flask, jsonify, request
 class Blockchain:
     def __init__(self):
         self.current_transactions = []
-        self.chain= []
+        self.chain = []
         self.new_block(proof=1, previous_hash=100)
+        self.nodes = set()
+
+    def register_node(self, node_address):
+        address = urlparse(node_address)
+        self.nodes.add(address)
+
+    def resolve_conflicts(self):
+        """
+        returns True if chain is replaced
+        """
+        max_length = len(self.chain)
+        new_chain = None
+
+        for neighbor in self.nodes:
+            neighbor = neighbor.geturl()
+            response = requests.get(f'{neighbor}/chain')
+            if response.json().get('length') > max_length and self.valid_chain(response.json().get('chain')):
+                new_chain = response.json().get('chain')
+                max_length = max_length
+
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+        return False
+
+
+
+    def valid_chain(self, chain):
+        """
+        checks if chain is valid
+        """
+        previous_block = chain[0]
+        index = 1
+        while index < len(chain):
+            block = chain[index]
+            if block['previous_hash'] != self.hash(previous_block):
+                return False
+            if not self.valid_proof(block['proof'], previous_block['proof']):
+                return False
+            index += 1
+            previous_block = block
+        return True
+
 
     def new_block(self, proof, previous_hash=None):
         block = {
@@ -126,5 +173,33 @@ def full_chain():
 
     return jsonify(response), 200
 
+@app.route('/nodes/register', methods=['POST'])
+def register():
+    values = request.get_json()
+    required = ['nodes']
+    if not all(k in required for k in values):
+        return "Input all args", 400
+    nodes = values.get('nodes')
+    for node in nodes:
+        blockchain.register_node(node)
+    return jsonify({ "message": f'Nodes {nodes} added to chain.'}), 201
+
+@app.route('/nodes/resolve', methods=['GET'])
+def resolve():
+   """
+   - call resolve_conflicts for each neighbor node
+   - if replaced, say it
+   """
+   replaced = blockchain.resolve_conflicts()
+   if replaced:
+       return jsonify({
+        "message": "Chain was replaced"
+       }), 200
+   else:
+       return jsonify({
+        "message": "No need to replace. Your chain is authoritative."
+       }), 200
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=int(sys.argv[1]))
